95959d469ed388792318a66402d3d6069a99f9a2
marklovejoydev <marklovejoydev@gmail.com>
2023-06-26 14:09:57 +0100

    will add updated test to test that it will not pass if given an empty array

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,15 @@
 const request = require("supertest");
 const app = require("../app");
 
+
+
+describe('404 will test for path not found',() =>{
+    test('status 404, responds with path not found when passed incorrect path',() =>{
+        return request(app)
+        .get('/api/notopics')
+        .expect(404)
+    })
+})
 describe('GET /api/topics', () => {
     test('status 200, responds with an array of objects with properties',() =>{
         return request(app)
@@ -8,15 +17,11 @@ describe('GET /api/topics', () => {
         .expect(200)
         .then(({body})=>{
             const { topics } =body
+            expect(topics).not.toHaveLength(0)
             topics.forEach(topic => {
                 expect(topic).toHaveProperty('description', expect.any(String))
                 expect(topic).toHaveProperty('slug', expect.any(String))
             });
         })
-    })
-    test('status 404, responds with path not found when passed incorrect path',() =>{
-        return request(app)
-        .get('/api/notopics')
-        .expect(404)
     })
 })
