692fd62c3989c2c5723f55121d5a7fed167a2a4c
marklovejoydev <marklovejoydev@gmail.com>
2023-06-27 12:31:30 +0100

    update for merger

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -100,7 +100,7 @@ describe('GET /api/articles', () => {
                 expect(article).toHaveProperty('created_at', expect.stringMatching(regEx));
                 expect(article).toHaveProperty('votes', expect.any(Number));
                 expect(article).toHaveProperty('article_img_url',expect.any(String));
-                expect(article).toHaveProperty('comment_count', expect.any(String));
+                expect(article).toHaveProperty('comment_count', expect.any(Number));
                 expect(article).not.toHaveProperty('body');
               });
             })
--- model/article-model.js
+++ model/article-model.js
@@ -13,7 +13,7 @@ exports.selectArticleById = (id) => {
 }
 
 exports.selectAllArticles = () => {
-let queryString = "SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url ORDER BY articles.created_at DESC;"
+let queryString = "SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, CAST(COUNT(comments.comment_id) AS INT) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url ORDER BY articles.created_at DESC;"
 return db.query(queryString).then(({ rows }) => {
   
   return rows
